8b1465bcc13fc0ffc620569cc6bdf59295b5cb29
Enzo-Lombardi <enzo.lombardi20@gmail.com>
2024-01-24 17:25:20 -0300

    Alterando numero secreto para 100 no JavaScrit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaNumero = [];
-let quantidadesNumeros = 10;
+let quantidadesNumeros = 100;
 let numeroSecreto = geraNumeroAleatorio();
 let tentativas = 1;
 
@@ -12,7 +12,7 @@ function exibirTextoNaTela(teg, texto) {
 
 function mensagemTela() {
     exibirTextoNaTela ("h1", "Jogo do número secreto");
-    exibirTextoNaTela ("p", "Escolha um número entre 1 e 10");
+    exibirTextoNaTela ("p", "Escolha um número entre 1 e 100");
 }
 
 mensagemTela();
